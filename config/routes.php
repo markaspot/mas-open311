76510bb7dca8aa05cad0376662a30b71b4318aa8
Holger Kreis <holger@markaspot.org>
2011-06-17 21:45:22 +0200

    config/routes.php

---
+++ config/routes.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * Routing for Open311 Plugin
+ */
+
+	Router::connect("/open311/requests",array('plugin' => 'open311' , 'controller' => 'open311','action' => 'requests', '[method]' => 'GET'));
+
+	Router::connect("/open311/services",array('plugin' => 'open311' , 'controller' => 'open311','action' => 'services', '[method]' => 'GET'));
+
+	Router::connect("/open311/requests/:id",array('plugin' => 'open311' , 'controller' => 'open311','action' => 'request', '[method]' => 'GET'),array('id' => '[0-9]+|[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}', 'pass' => array('id')));
+
+	Router::connect("/open311/requests",array('plugin' => 'open311' ,'controller' => 'open311','action' => 'add', '[method]' => 'POST'),array('id' => '[0-9]+|[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}', 'pass' => array('id')));
+	
+	
+	//Router::connect("/open311/service/",array('controller' => 'open311', "action" => "service", "[method]" => "GET"));
+
+?>
