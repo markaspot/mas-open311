f25111ccf68eaad0bf8aa5020c3b0205fbbc7426
Holger Kreis <holger@markaspot.org>
2011-06-23 13:17:33 +0200

    add json support

---
+++ config/open311.php
@@ -0,0 +1,17 @@
+<?php
+
+$config['open311']['jurisdiction_id'] = "mas-city.com";
+
+// changeset - String - Sortable field that specifies the last time this document was updated.
+$config['open311']['changeset'] = "2011-06-21 00:00";
+// contact - String - Human readable information on how to get more information on this provider.
+$config['open311']['contact'] = "You can email or call for assistance api@mas-city.com +1 (555) 555-5555";
+// key_service - String - Human readable information on how to get an API key.
+$config['open311']['key_service'] = "You can request a key here: http://api.mas-city.com/api_key/request<";
+//endpoints.endpoint.url - String - URL of the endpoint provider
+$config['open311']['url'] = "http://mas-city.com/open311"; 
+
+$config['open311']['type'] = "test"; // production, test, dev
+$config['open311']['formats'] = array('format'=>'text/xml','format' => 'application/json');
+
+?>
---
+++ views/open311/xml/discovery.ctp
@@ -0,0 +1,3 @@
+<?php
+echo $this->Xml->serialize($discovery, array('format' => 'tags', 'root' => 'discovery')); 
+?>
